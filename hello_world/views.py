9330c5510bc2cd36c30d6c9cbf549f227b9f8c9b
Darek <darekkob@op.pl>
2020-05-16 13:22:22 +0200

    poprawa travis

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Stefan"
+moje_imie = "Dude"
 msg = "Hello World!"
 
 
